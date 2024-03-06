5e62878fb529433031b608ed3ffc4623e0f571ec
김민주 <mj1584.mk@gmail.com>
2024-03-06 17:02:50 +0900

    0306 algorithm_study PS on my desktop

---
+++ src/algorithm_study/day_03_06/BOJ_1647.java
@@ -0,0 +1,10 @@
+package algorithm_study.day_03_06;
+
+public class BOJ_1647 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/algorithm_study/day_03_06/BOJ_1976.java
@@ -0,0 +1,17 @@
+package algorithm_study.day_03_06;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1976 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		int N = Integer.parseInt(br.readLine());
+	}
+	
+}

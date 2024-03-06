b75a5abb4fa6d128c9d39c1e9be785739f5342e4
김민주 <mj1584.mk@gmail.com>
2024-03-06 17:03:43 +0900

    0306 algorithm_study PS on my desktop

--- src/algorithm_study/day_03_06/BOJ_1976.java
+++ src/algorithm_study/day_03_06/BOJ_1976.java
@@ -3,6 +3,7 @@ package algorithm_study.day_03_06;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ_1976 {
@@ -11,7 +12,16 @@ public class BOJ_1976 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
 		
-		int N = Integer.parseInt(br.readLine());
+		int V = Integer.parseInt(br.readLine());
+		int E = Integer.parseInt(br.readLine());
+		
+		int[][] adjMatrix = new int[V][V];
+		
+		for (int i = 0; i < V; i++) {
+			adjMatrix[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		}
+		
+		
 	}
 	
 }

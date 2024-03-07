fe47ac36dffb320d1c255478e2d403c0fd34551e
김민주 <mj1584.mk@gmail.com>
2024-03-07 17:22:54 +0900

    0307 algorithm_study PS on my desktop

--- src/Test.java
+++ src/Test.java
@@ -3,16 +3,8 @@ import java.util.Arrays;
 public class Test {
 
 	public static void main(String[] args) {
-		boolean [] A = {true, false};
-		boolean [] B = {true, true};
-		int hit = 2;
-		int base = 8;
-		int score = 0;
-		base = base << hit;
-		base = base | (1 << hit - 1);
-		System.out.println(base);
-		if((base & 32) == 1) score++; 
-		System.out.println(base & 32);
+		String num = "0100";
+		System.out.println(Integer.parseInt(num));
 	}
 
 }
---
+++ src/algorithm_study/day_03_07/BOJ_10844.java
@@ -0,0 +1,10 @@
+package algorithm_study.day_03_07;
+
+public class BOJ_10844 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

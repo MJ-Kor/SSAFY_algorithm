a443fe254365a2a46730353a5fcecf0dd17e2e16
MJ-Kor <mj1584.mk@gmail.com>
2024-03-12 21:54:32 +0900

    0312 algorithm study PS on my notebook

--- src/algorithm_study/day_03_12/BOJ_4179.java
+++ src/algorithm_study/day_03_12/BOJ_4179.java
@@ -12,17 +12,18 @@ import java.util.StringTokenizer;
 public class BOJ_4179 {
 
 	private static class Point{
-		int r, c;
+		int r, c, jf;
 
-		public Point(int r, int c) {
+		public Point(int r, int c, int jf) {
 			super();
 			this.r = r;
 			this.c = c;
+			this.jf = jf;
 		}
 
 		@Override
 		public String toString() {
-			return "Point [r=" + r + ", c=" + c + "]";
+			return "Point [r=" + r + ", c=" + c + ", jf=" + jf + "]";
 		}
 		
 	}
@@ -36,11 +37,12 @@ public class BOJ_4179 {
 			this.point = point;
 			this.breadth = breadth;
 		}
-		
+
 		@Override
 		public String toString() {
 			return "QueueData [point=" + point + ", breadth=" + breadth + "]";
 		}
+		
 	}
 	
 	private static int[] dr = {-1, 0, 1, 0};
@@ -61,19 +63,61 @@ public class BOJ_4179 {
 			String line = br.readLine();
 			for (int c = 0; c < C; c++) {
 				char in = line.charAt(c);
-				if(in == 'J') jihoon = new Point(r, c);
-				if(in == 'F') fires.add(new Point(r, c));
+				if(in == 'J') jihoon = new Point(r, c, 0);
+				if(in == 'F') fires.add(new Point(r, c, 1));
+				map[r][c] = in;
 			}
 		}
 		
-		bfs(R, C, jihoon, fires);
+		bfs(R, C, jihoon, fires, map);
 	}
 
-	private static void bfs(int r, int c, Point jihoon, List<Point> fires) {
-		boolean[][] visited = new boolean[r][c];
+	private static void bfs(int R, int C, Point jihoon, List<Point> fires, char[][] map) {
+		boolean[][][] visited = new boolean[2][R][C];
 		Queue<QueueData> queue = new ArrayDeque<>();
-		visited[jihoon.r][jihoon.c] = true;
 		
+		queueInit(queue, jihoon, fires, visited);
+		while(!queue.isEmpty()) {
+			QueueData curr = queue.poll();
+			if(curr.point.jf == 1) {
+				int r = curr.point.r;
+				int c = curr.point.c;
+				for (int i = 0; i < 4; i++) {
+					int nr = r + dr[i];
+					int nc = c + dc[i];
+					if(nr >= 0 && nr < R && nc >= 0 && nc < C && !visited[1][nr][nc] && map[nr][nc] != '#') {
+						visited[1][nr][nc] = true;
+						queue.offer(new QueueData(new Point(nr, nc, curr.point.jf), curr.breadth + 1));
+					}
+				}
+			}
+			if(curr.point.jf == 0) {
+				int r = curr.point.r;
+				int c = curr.point.c;
+				for (int i = 0; i < 4; i++) {
+					int nr = r + dr[i];
+					int nc = c + dc[i];
+					if(nr < 0 || nr >= R || nc < 0 || nc >= C) {
+						System.out.println(curr.breadth + 1);
+						System.exit(0);
+					}
+					if(nr >= 0 && nr < R && nc >= 0 && nc < C && !visited[1][nr][nc] && !visited[0][nr][nc] && map[nr][nc] != '#') {
+						visited[0][nr][nc] = true;
+						queue.offer(new QueueData(new Point(nr, nc, curr.point.jf), curr.breadth + 1));
+					}
+				}
+			}
+		}
+		System.out.println("IMPOSSIBLE");
+	}
+
+	private static void queueInit(Queue<QueueData> queue, Point jihoon, List<Point> fires, boolean[][][] visited) {
+		for(Point fire : fires) {
+			queue.offer(new QueueData(fire, 0));
+			visited[1][fire.r][fire.c] = true;
+		}
+		queue.offer(new QueueData(jihoon, 0));
+		visited[0][jihoon.r][jihoon.c] = true;
 	}
 	
 }

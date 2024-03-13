49d9467cd46ad426532ea22b16aa004d290f9e11
김민주 <mj1584.mk@gmail.com>
2024-03-14 01:51:06 +0900

    0313 algorithm study PS on my desktop

--- src/algorithm_study/day_03_13/BOJ_1038.java
+++ src/algorithm_study/day_03_13/BOJ_1038.java
@@ -1,10 +1,37 @@
 package algorithm_study.day_03_13;
 
-public class BOJ_1038 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class BOJ_1038 {    
+    
+    private static ArrayList<Long> list;
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        list = new ArrayList<>();
+        
+        if(N <= 10) System.out.println(N);
+        else if(N > 1022) System.out.println("-1");
+        else {
+            for(int i = 0; i < 10; i++) {
+                calculate(i, 1);
+            }
+            Collections.sort(list);
+            System.out.println(list.get(N));
+        }
+    }    
+    
+    public static void calculate(long num, int idx) {
+        if(idx > 10) return;
+        list.add(num);
+        for(int i = 0; i < num % 10; i++) {
+            calculate((num * 10) + i, idx + 1);
+        }
+    }
 }
--- src/algorithm_study/day_03_13/BOJ_2251.java
+++ src/algorithm_study/day_03_13/BOJ_2251.java
@@ -3,44 +3,68 @@ package algorithm_study.day_03_13;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.Set;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class BOJ_2251 {
 
-	private static boolean[][][] visited;
-	private static PriorityQueue<Integer> pq = new PriorityQueue<>();
-	private static int A, B, C, a, b, c;
-	
+	private static int[] bucket;
+	private static boolean[][] check;
+	private static Set<Integer> answer;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		A = Integer.parseInt(st.nextToken());
-		B = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		
-		visited = new boolean[A + 1][B + 1][C + 1];
-		
-		a = 0;
-		b = 0;
-		c = C;
-		
-		dfs(c, b, 0);
-		dfs(c, b, 1);
-		dfs(c, a, 0);
-		dfs(c, a, 1);
+
+		bucket = new int[3];
+		check = new boolean[201][201];
+		for (int i = 0; i < 3; i++) {
+			bucket[i] = Integer.parseInt(st.nextToken());
+		}
+
+		answer = new TreeSet<>();
+		dfs(0, 0, bucket[2]);
+
+		for (int num : answer) {
+			System.out.print(num + " ");
+		}
 	}
 
-	private static void dfs(int start, int end, int status) {
-		
-		if(status == 0) {
-			
+	static void dfs(int a, int b, int c) {
+		if (check[a][b])
+			return;
+
+		if (a == 0) {
+			answer.add(c);
 		}
-		
-		// start -> end1로 옮기는 경우 (start > end1)
-		dfs(a, B, C - B);
-		// start -> end1로 옮기는 경우 (start <= end1)
-		dfs(a, C, 0);
-		// start -> end2로 옮기는 경우 (start > 
+		check[a][b] = true;
+
+		if (a + b > bucket[1]) {
+			dfs((a + b) - bucket[1], bucket[1], c);
+		} else {
+			dfs(0, a + b, c);
+		}
+
+		if (a + b > bucket[0]) {
+			dfs(bucket[0], a + b - bucket[0], c);
+		} else {
+			dfs(a + b, 0, c);
+		}
+
+		if (a + c > bucket[0]) {
+			dfs(bucket[0], b, a + c - bucket[0]);
+		} else {
+			dfs(a + c, b, 0);
+		}
+
+		if (b + c > bucket[1]) {
+			dfs(a, bucket[1], b + c - bucket[1]);
+		} else {
+			dfs(a, b + c, 0);
+		}
+
+		dfs(a, 0, b + c);
+		dfs(0, b, a + c);
 	}
 }

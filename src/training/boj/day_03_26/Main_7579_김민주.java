4da8e2d95b79e57f320b8d043940d5657a70b0ae
김민주 <mj1584.mk@gmail.com>
2024-03-26 23:56:26 +0900

    0326 algorithm PS on my desktop

--- src/training/boj/day_03_26/Main_7579_김민주.java
+++ src/training/boj/day_03_26/Main_7579_김민주.java
@@ -13,18 +13,47 @@ public class Main_7579_김민주 {
 		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
+		int sum = Integer.MAX_VALUE;
 		
 		int[] m = new int[N];
 		int[] c = new int[N];
-		int sum = 0;
+		int[][] dp = new int[N][10001];
+		
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			m[i] = Integer.parseInt(st.nextToken());
-			sum += m[i];
 		}
-		for (int i = 0; i < c.length; i++) {
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
 			c[i] = Integer.parseInt(st.nextToken());
 		}
+		
+		
+		for (int i = 0; i < N; i++) {
+			int cost = c[i];
+			int memory = m[i];
+			
+			for (int j = 0; j <= 10000; j++) {
+				if(i == 0) {
+					if(j >= cost) {
+						dp[i][j] = memory;
+					}
+				} 
+				else {
+					if(j >= cost) {
+						dp[i][j] = Math.max(dp[i - 1][j - cost] + memory, dp[i - 1][j]);
+					} else {
+						dp[i][j] = dp[i - 1][j];	
+					}
+				}
+				
+				if(dp[i][j] >= M) {
+					sum = Math.min(sum, j);
+				}
+			}
+		}
+		
+		System.out.println(sum);
 	}
 
 }

88816dc054e4be66db65540802daabf97e52a7b0
MJ-Kor <mj1584.mk@gmail.com>
2024-03-28 21:34:03 +0900

    0327 PS on my notebook

--- src/algorithm_study/day_03_28/BOJ_15685.java
+++ src/algorithm_study/day_03_28/BOJ_15685.java
@@ -26,6 +26,9 @@ public class BOJ_15685 {
 	
 	private static int[] dx = {1, 0, -1, 0};
 	private static int[] dy = {0, -1, 0, 1};
+	private static int[] sx = {1, 0, 1};
+	private static int[] sy = {0, 1, 1};
+	
 	private static int[][] map;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -41,12 +44,35 @@ public class BOJ_15685 {
 			drawDragonCurve(x, y, d, g);
 		}
 		
-		for (int i = 0; i <=100; i++) {
-			for (int j = 0; j <=100; j++) {
-				System.out.print(map[j][i] + " ");
+//		for (int i = 0; i <=100; i++) {
+//			for (int j = 0; j <=100; j++) {
+//				System.out.print(map[j][i] + " ");
+//			}
+//			System.out.println();
+//		}
+		
+		int result = countSqure();
+		System.out.println(result);
+		
+	}
+
+	private static int countSqure() {
+		int cnt = 0;
+		for (int i = 0; i < 100; i++) {
+			for (int j = 0; j < 100; j++) {
+				if(map[j][i] == 1) {
+					boolean flag = true;
+					for (int k = 0; k < 3; k++) {
+						if(map[j + sy[k]][i + sx[k]] != 1) {
+							flag = false;
+							break;
+						}
+					}
+					if(flag) cnt++;
+				}
 			}
-			System.out.println();
 		}
+		return cnt;
 	}
 
 	private static void drawDragonCurve(int x, int y, int d, int g) {
@@ -58,15 +84,15 @@ public class BOJ_15685 {
 		dragonCurve.add(new Point((x + dx[d]), (y + dy[d])));
 		
 		for (int generation = 1; generation <= g; generation++) {
-			System.out.println("generation: " + generation);
+//			System.out.println("generation: " + generation);
 			// 기존 세대 드래곤 커브의 끝 index
 			int end = dragonCurve.size() - 1;
-			System.out.println(dragonCurve.toString());
+//			System.out.println(dragonCurve.toString());
 			int oldIdx = end;
 			int newIdx = end;
-			// 다음 세대 드래곤 커브 그리를 방법
+			// 다음 세대 드래곤 커브 그리는 방법
 			// 기존 세대 드래곤 커브의 끝에서 시작, 이를 end라고 하자
-			// 앞으로 그릴 드래곤 커브의 시작 점 역시 end 위치다. 
+			// 앞으로 그릴 드래곤 커브의 시작 점 역시 end 위치
 			// 따라서 기존 드래곤 커브의 좌표를 oldIdx, 그릴 드래곤 커브의 시작 좌표를 newIdx라고 하면
 			// 시작은 oldIdx = newIdx = end;
 			// 1. oldIdx, oldIdx - 1가 이루는 방향 D구하기
@@ -78,10 +104,10 @@ public class BOJ_15685 {
 				
 				// 1.
 				Point op = dragonCurve.get(oldIdx);
-				System.out.println("op: " + op);
+//				System.out.println("op: " + op);
 				oldIdx -= 1;
 				Point nop = dragonCurve.get(oldIdx);
-				System.out.println("nop: " + nop);
+//				System.out.println("nop: " + nop);
 				int odx = nop.x - op.x;
 				int ody = nop.y - op.y;
 				int D = 0; 
@@ -93,10 +119,10 @@ public class BOJ_15685 {
 				if(nD == -1) nD += 4;
 				// 3.
 				Point np = dragonCurve.get(newIdx);
-				System.out.println("np: " + np);
-				System.out.println("nD: " + nD);
+//				System.out.println("np: " + np);
+//				System.out.println("nD: " + nD);
 				Point nnp = new Point((np.x + dx[nD]), (np.y + dy[nD]));
-				System.out.println("nnp: " + nnp);
+//				System.out.println("nnp: " + nnp);
 				// 4.
 				dragonCurve.add(nnp);
 				
@@ -104,7 +130,7 @@ public class BOJ_15685 {
 			}
 		}
 		
-		System.out.println(dragonCurve.toString());
+//		System.out.println(dragonCurve.toString());
 		
 		for(Point p : dragonCurve) {
 			map[p.x][p.y] = 1;

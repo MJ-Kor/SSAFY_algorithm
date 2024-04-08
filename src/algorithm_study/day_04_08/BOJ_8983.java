0b61d6df835596387d960a7dfda8ee68cbf2dc27
김민주 <mj1584.mk@gmail.com>
2024-04-09 01:35:44 +0900

    0408 PS on my desktop

--- src/algorithm_study/day_04_08/BOJ_8983.java
+++ src/algorithm_study/day_04_08/BOJ_8983.java
@@ -3,6 +3,7 @@ package algorithm_study.day_04_08;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ_8983 {
@@ -26,6 +27,8 @@ public class BOJ_8983 {
 			rowLoc[i] = Integer.parseInt(st.nextToken());
 		}
 		
+		Arrays.sort(rowLoc);
+		
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int r = Integer.parseInt(st.nextToken());
@@ -36,27 +39,18 @@ public class BOJ_8983 {
 	}
 
 	private static void hunt(int start, int end, int r, int c) {
-		int mid = (start + end) / 2;
-		
-		if(start < end) {
-			if(L - Math.abs(r - rowLoc[end]) <= c) result++;
-			return;
-		}
-	
-		if(Math.abs(start - end) == 1) {
-			if(L - Math.min(Math.abs(rowLoc[start]-r), Math.abs(rowLoc[end]-r)) <= c) result++;
-			return;
-		}
-		
-		if(rowLoc[mid] > r) {
-			hunt(mid + 1, end, r, c);
-		}
-		else if(rowLoc[mid] < r) {
-			hunt(start, mid - 1, r, c);
-		}
-		else {
-			if(c <= L) result++;
-			return;
+		while(start <= end) {
+			int mid = (start + end) / 2;
+			long dist = Math.abs(rowLoc[mid] - r) + c;
+			if(dist <= L) {
+				result++;
+				break;
+			}
+			if(r < rowLoc[mid]) {
+				end = mid - 1;
+			} else {
+				start = mid + 1;
+			}
 		}
 	}
 }

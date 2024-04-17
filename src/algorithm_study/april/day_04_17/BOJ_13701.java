73bcfd70d00482cf5b03ec0b59872925d1840ae0
김민주 <mj1584.mk@gmail.com>
2024-04-18 01:02:35 +0900

    0417 PS on my desktop

--- src/algorithm_study/april/day_04_17/BOJ_13701.java
+++ src/algorithm_study/april/day_04_17/BOJ_13701.java
@@ -3,8 +3,7 @@ package algorithm_study.april.day_04_17;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.BitSet;
 import java.util.StringTokenizer;
 
 public class BOJ_13701 {
@@ -13,14 +12,18 @@ public class BOJ_13701 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		Set<Integer> set = new LinkedHashSet<>();
-		while(st.hasMoreTokens()) {
-			set.add(Integer.parseInt(st.nextToken()));
-		}
+		BitSet set = new BitSet();
 		
-		for(int e : set) {
-			System.out.print(e + " ");
+		StringBuilder sb = new StringBuilder();
+		
+		while(st.hasMoreTokens()) {
+			int num = Integer.parseInt(st.nextToken());
+			if(!set.get(num)) {
+				set.set(num);
+				sb.append(num).append(" ");
+			}
 		}
+		System.out.println(sb.toString().trim());
 	}
 
 }

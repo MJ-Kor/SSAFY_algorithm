f98a0e15f3af26e74958f65ba8f77bc857f81cc3
김민주 <mj1584.mk@gmail.com>
2024-07-12 01:15:08 +0900

    0712 PS on my desktop

--- src/algorithm_study/july/day_07_11/BOJ_15724.java
+++ src/algorithm_study/july/day_07_11/BOJ_15724.java
@@ -10,6 +10,35 @@ public class BOJ_15724 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        
+        int R = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int[][] map = new int[R + 1][C + 1];
+
+        for (int r = 1; r <= R; r++) {
+            st = new StringTokenizer(br.readLine());
+            for (int c = 1; c <= C; c++) {
+
+                if (r == 1) {
+                    if (c == 1) map[r][c] = Integer.parseInt(st.nextToken());
+                    else map[r][c] = map[r][c - 1] + Integer.parseInt(st.nextToken());
+                } else {
+                    if (c == 1) map[r][c] = map[r - 1][c] + Integer.parseInt(st.nextToken());
+                    else map[r][c] = map[r - 1][c] + map[r][c - 1] - map[r - 1][c - 1] + Integer.parseInt(st.nextToken());
+                }
+
+            }
+        }
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            System.out.println(map[x2][y2] - map[x1 - 1][y2] - map[x2][y1 - 1] + map[x1 - 1][y1 - 1]);
+        }
     }
 }

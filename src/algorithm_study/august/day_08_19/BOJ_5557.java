2e5961f6cd58f979c4f05181b55e0fab95452e99
김민주 <mj1584.mk@gmail.com>
2024-08-19 17:51:40 +0900

    0819 PS on SSAFY notebook

---
+++ src/algorithm_study/august/day_08_19/BOJ_5557.java
@@ -0,0 +1,52 @@
+package algorithm_study.august.day_08_19;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_5557 {
+
+    static int answer = 0;
+    static int N;
+    static int[] numbers = new int[100];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(0, 0, numbers[0]);
+
+
+        System.out.println(answer);
+
+    }
+
+
+    static void dfs(int depth, int res, int num) {
+
+        if(res < 0 || res >= 20) return;
+
+        if(depth == N - 1) {
+            if(res == num) {
+                answer++;
+            }
+            return;
+        }
+
+        res += num;
+        dfs(depth + 1, res, numbers[depth + 1]);
+
+        res -= (2 * num);
+        dfs(depth + 1, res, numbers[depth + 1]);
+
+    }
+}

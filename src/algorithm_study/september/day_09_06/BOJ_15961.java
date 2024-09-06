bda2158e11b52f85f4c76a83d8c75c2c70577551
김민주 <mj1584.mk@gmail.com>
2024-09-06 17:58:10 +0900

    0906 PS on ssafy notebook

---
+++ src/algorithm_study/september/day_09_06/BOJ_15961.java
@@ -0,0 +1,4 @@
+package algorithm_study.september.day_09_06;
+
+public class BOJ_15961 {
+}
---
+++ src/training/boj/extra/gold/Main_16724_김민주.java
@@ -0,0 +1,16 @@
+package training.boj.extra.gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_16724_김민주 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+    }
+}

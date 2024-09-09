051cf9e79f88061a77a4fa973cfa963a1db968f4
김민주 <mj1584.mk@gmail.com>
2024-09-10 00:56:09 +0900

    update BinarySearch

--- src/templates/algorithms/BinarySearch.java
+++ src/templates/algorithms/BinarySearch.java
@@ -13,8 +13,8 @@ public class BinarySearch {
 		int find = 9;
 		
 		long beforeTime = System.currentTimeMillis();
-//		int idx = recursionBinarySearch(find, 0, 4, 8, arr);
-		int idx = recursionBinarySearch(find, 0, 8, arr);
+		int idx = recursionBinarySearch(find, 0, 4, 8, arr);
+//		int idx = recursionBinarySearch(find, 0, 8, arr);
 		long afterTime = System.currentTimeMillis();
 		System.out.println("재귀 함수를 사용한 이분 탐색" + idx);
 		System.out.println("찾으려는 수: " + find + ", 위치: " + idx);
@@ -23,40 +23,40 @@ public class BinarySearch {
 	}
 
 	
-//	private static int recursionBinarySearch(int value, int start, int mid, int end, int[] arr) {
-//		
-//		if(arr[mid] == value) return mid;
-//		
-//		if(start == end) return -1;
-//		
-//		if(arr[mid] < value) {
-//			int nStart = mid + 1;
-//			int nMid = ((mid + 1) + end) / 2;
-//			return recursionBinarySearch(value, nStart, nMid, end, arr);
-//		}
-//		else {
-//			int nMid = (start + mid) / 2;
-//			int nEnd = mid - 1;
-//			return recursionBinarySearch(value, start, nMid, nEnd, arr);
-//		}
-//	}
+	private static int recursionBinarySearch(int value, int start, int mid, int end, int[] arr) {
 
-	private static int recursionBinarySearch(int value, int start, int end, int[] arr) {
-		
-		if (start > end) return -1;
-		
-		int mid = (start / end) / 2;
-		
-		if (arr[mid] == value) return mid;
-		
+		if(arr[mid] == value) return mid;
+
+		if(start == end) return -1;
+
 		if(arr[mid] < value) {
 			int nStart = mid + 1;
-			return recursionBinarySearch(value, nStart, end, arr);
+			int nMid = ((mid + 1) + end) / 2;
+			return recursionBinarySearch(value, nStart, nMid, end, arr);
 		}
 		else {
+			int nMid = (start + mid) / 2;
 			int nEnd = mid - 1;
-			return recursionBinarySearch(value, start, nEnd, arr);
+			return recursionBinarySearch(value, start, nMid, nEnd, arr);
 		}
 	}
+
+//	private static int recursionBinarySearch(int value, int start, int end, int[] arr) {
+//
+//		if (start > end) return -1;
+//
+//		int mid = (start / end) / 2;
+//
+//		if (arr[mid] == value) return mid;
+//
+//		if(arr[mid] < value) {
+//			int nStart = mid + 1;
+//			return recursionBinarySearch(value, nStart, end, arr);
+//		}
+//		else {
+//			int nEnd = mid - 1;
+//			return recursionBinarySearch(value, start, nEnd, arr);
+//		}
+//	}
 	
 }

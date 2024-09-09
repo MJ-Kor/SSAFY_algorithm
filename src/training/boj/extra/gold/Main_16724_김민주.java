bd080f33ef1b33df2097c890837f3569a1b16ccf
김민주 <mj1584.mk@gmail.com>
2024-09-10 00:55:01 +0900

    0909 PS on my desktop

--- src/training/boj/extra/gold/Main_16724_김민주.java
+++ src/training/boj/extra/gold/Main_16724_김민주.java
@@ -7,10 +7,66 @@ import java.util.StringTokenizer;
 
 public class Main_16724_김민주 {
 
+    static int[][] map;
+    static boolean[][] visited;
+    static boolean[][] finished;
+    static int answer = 0;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        visited = new boolean[N][M];
+        finished = new boolean[N][M];
+
+        for(int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for(int j = 0; j < M; j++) {
+                if(str.charAt(j) == 'U') {
+                    map[i][j] = 0;
+                } else if(str.charAt(j) == 'D') {
+                    map[i][j] = 1;
+                } else if(str.charAt(j) == 'L') {
+                    map[i][j] = 2;
+                } else {
+                    map[i][j] = 3;
+                }
+            }
+        }
+
+        for(int r = 0; r < N; r++) {
+            for(int c = 0; c < M; c++) {
+                if(visited[r][c] != true) {
+                    dfs(r, c);
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    public static void dfs(int r, int c) {
+
+        int now = map[r][c];
+        visited[r][c] = true;
+        int nr = r + dr[now];
+        int nc = c + dc[now];
+
+        if(!visited[nr][nc]) {
+            dfs(nr, nc);
+        } else{
+            if(!finished[nr][nc]){
+                answer++;
+            }
+        }
+
+        finished[r][c] = true;
     }
 }

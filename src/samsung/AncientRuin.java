b7289133a7f95b950560807ad17bc809e0875469
김민주 <mj1584.mk@gmail.com>
2024-10-10 03:59:35 +0900

    1009 삼성 코테 준비 on my desktop

--- src/samsung/AncientRuin.java
+++ src/samsung/AncientRuin.java
@@ -3,30 +3,11 @@ package samsung;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 import java.util.StringTokenizer;
-
-// [1] 탐사
-// 5x5 격자 주어짐
-// 3x3 격자 회전
-// 90도, 180도, 270도 중 시계 방향 회전
-// 1. 유물 가치 최대화
-// 2. 회전 각도 가장 작음
-// 3. 회전 중심 좌표의 열이 가장 작은 구간
-// 4. 회전 중심 좌표의 행이 가장 작은 구간
-
-// [2] 유물 획득
-// 상하좌우로 인접한 같은 종류의 유물 조각은 연결
-// 3개 이상 연결된 경우, 유물이 되어 사라짐
-// 유물의 가치는 조각의 개수 -> 3개 이상 연결된 영역을 찾고, 영역의 칸 개수 구하기
-// 유적의 벽면에 적힌 순서대로 유물이 없어진 곳에 새로운 조각이 생겨남 -> 없어진 곳의 위치를 구해놔야 함
-// 1. 열 번호가 작은 순
-// 2. 행 번호가 큰 순
-// 유적의 벽면에 있는 숫자는 남는 숫자부터 순서대로 사용 -> 사용된 숫자 위치 인덱스 처리
-// 유물 획득이 더이상 진행되지 않을때까지 반복 (유물의 연쇄 획득)
-
-// [3] 탐사 반복
-// [1] ~ [2]를 1턴으로 생각하여 K번 진행
-// K번을 진행하지 못해도 더이상 유물을 획득할 수 없다면 모든 탐사는 종류 -> 그대로 끝, 종료되는 턴에는 출력하지 않음
 
 class RuinsStatus {
 	int r, c, rotateAngle, value;
@@ -47,6 +28,23 @@ class RuinsStatus {
 	
 }
 
+class Disk {
+	int r, c, type;
+
+	public Disk(int r, int c, int type) {
+		super();
+		this.r = r;
+		this.c = c;
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "Disk [r=" + r + ", c=" + c + ", type=" + type + "]";
+	}
+	
+}
+
 public class AncientRuin {
 
 	static int RUINSSIZE = 5;
@@ -55,19 +53,26 @@ public class AncientRuin {
 	static StringTokenizer st;
 	static int K, M, wallIdx = 0;
 	static int[] wall;
+	static int[] dr = {-1, -1, -1, 0, 1, 1, 1, 0};
+	static int[] dc = {-1, 0, 1, 1, 1, 0, -1, -1};
 	static int[][] ruinsInfo;
+	static int[][] copyOfRuinsInfo;
+	static boolean[][] visited;
+	static boolean[][] isEmptyDisk;
 	static RuinsStatus ruinsStatus;
 	
 	public static void main(String[] args) throws IOException {
 		
 		init();
 		solution();
-		
 	}
 	
 	private static void init() throws IOException {
 
 		ruinsInfo = new int[RUINSSIZE][RUINSSIZE];
+		visited = new boolean[RUINSSIZE][RUINSSIZE];
+		isEmptyDisk = new boolean[RUINSSIZE][RUINSSIZE];
+		copyOfRuinsInfo = copyRuinsInfo();
 		st = new StringTokenizer(br.readLine());
 		K = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
@@ -88,16 +93,36 @@ public class AncientRuin {
 		
 		ruinsStatus = new RuinsStatus(0, 0, 0, 0);
 	}
-	
+
 	private static void solution() {
 		
-		// 회전 후 1차 유물 획득 - 27번 반복
-		findRuins();
-		getValue();
-		fillRelics();
-		exploration();
-		// 제일 큰 경우 선택
-		// 유물 획득 반복 - 가치 저장
+		for (int k = 0; k < K; k++) {
+			copyOfRuinsInfo = copyRuinsInfo();
+			ruinsStatus = new RuinsStatus(0, 0, 0, 0);
+			int answer = 0;
+			findRuins();
+			int[] rotateLine = new int[8];
+			setRotateLine(rotateLine, ruinsStatus.r, ruinsStatus.c);
+			if(ruinsStatus.rotateAngle == 90) {
+				rotate90(rotateLine, ruinsStatus.r, ruinsStatus.c);				
+			} else if(ruinsStatus.rotateAngle == 180) {
+				rotate180(rotateLine, ruinsStatus.r, ruinsStatus.c);
+			} else {
+				rotate270(rotateLine, ruinsStatus.r, ruinsStatus.c);
+			}
+			while(true) {
+				int value = getValue();
+				System.out.println("value: " + value);
+				answer += value;
+				if(value == 0) break;
+				fillRelics();
+			}
+			System.out.println(answer);
+			
+			if(answer == 0) return;
+			
+			System.out.println(answer);
+		}
 		
 	}
 
@@ -115,16 +140,48 @@ public class AncientRuin {
 		int[] rotateLine = new int[8];
 		setRotateLine(rotateLine, r, c);
 		// rotate -> rotateLine을 각도만큼 밀어버리기
-		rotate90(r, c);
+		rotate90(rotateLine, r, c);
 		compare(90, r, c);
-		rotate180(r, c);
+		rotate180(rotateLine, r, c);
 		compare(180, r, c);
-		rotate270(r, c);
+		rotate270(rotateLine, r, c);
 		compare(270, r, c);
+		rotate360(rotateLine, r, c);
 	}
 
 	private static void setRotateLine(int[] rotateLine, int r, int c) {
 		
+		for (int i = 0; i < 8; i++) {
+			rotateLine[i] = copyOfRuinsInfo[r + dr[i]][c + dc[i]];
+		}
+	}
+
+	private static void rotate90(int[] rotateLine, int r, int c) {
+		
+		for(int i = 0; i < 8; i++) {
+			copyOfRuinsInfo[r + dr[i]][c + dc[i]] = rotateLine[(i + 2) % 8];
+		}
+	}
+	
+	private static void rotate180(int[] rotateLine, int r, int c) {
+		
+		for(int i = 0; i < 8; i++) {
+			copyOfRuinsInfo[r + dr[i]][c + dc[i]] = rotateLine[(i + 4) % 8];
+		}
+	}
+	
+	private static void rotate270(int[] rotateLine, int r, int c) {
+		
+		for(int i = 0; i < 8; i++) {
+			copyOfRuinsInfo[r + dr[i]][c + dc[i]] = rotateLine[(i + 6) % 8];
+		}
+	}
+	
+	private static void rotate360(int[] rotateLine, int r, int c) {
+		
+		for(int i = 0; i < 8; i++) {
+			copyOfRuinsInfo[r + dr[i]][c + dc[i]] = rotateLine[i];
+		}
 	}
 
 	private static void compare(int rotateAngle, int r, int c) {
@@ -161,36 +218,79 @@ public class AncientRuin {
 		ruinsStatus.c = c;
 	}
 
-	private static void rotate90(int r, int c) {
-		
-		
-	}
-
-	private static void rotate180(int r, int c) {
-		
-		
-	}
-
-	private static void rotate270(int r, int c) {
-		// TODO Auto-generated method stub
-		
-	}
-
 	private static int getValue() {
-		// TODO Auto-generated method stub
+		int totalValue = 0;
+		
+		for (int r = 0; r < RUINSSIZE; r++) {
+			for (int c = 0; c < RUINSSIZE; c++) {
+				if(!visited[r][c]) {
+					totalValue += bfs(r, c);
+				}
+			}
+		}
+		
+		visited = new boolean[RUINSSIZE][RUINSSIZE];
+		return totalValue;
+	}
+
+	private static int bfs(int r, int c) {
+		
+		Queue<Disk> queue = new LinkedList<>();
+		Stack<Disk> stack = new Stack<Disk>(); 
+		queue.add(new Disk(r, c, copyOfRuinsInfo[r][c]));
+		visited[r][c] = true;
+		
+		while(!queue.isEmpty()) {
+			int nr, nc;
+			Disk curr = queue.poll();
+			stack.add(curr);
+			
+			for (int i = 1; i < 8; i+=2) {
+				nr = curr.r + dr[i];
+				nc = curr.c + dc[i];
+				if(nr >= 0 && nr < RUINSSIZE && nc >=0 && nc < RUINSSIZE && !visited[nr][nc] && copyOfRuinsInfo[nr][nc] == curr.type) {
+					queue.add(new Disk(nr, nc, copyOfRuinsInfo[nr][nc]));
+					visited[nr][nc] = true;
+				}
+			}
+		}
+		
+		if(stack.size() >= 3) {
+			int size = stack.size();
+			
+			while(!stack.isEmpty()) {
+				Disk disk = stack.pop();
+				isEmptyDisk[disk.r][disk.c] = true;
+			}
+			return size;
+		}
+		
 		return 0;
 	}
 
 	private static void fillRelics() {
-		// TODO Auto-generated method stub
 		
+		for (int c = 0; c < RUINSSIZE; c++) {
+			for (int r = RUINSSIZE - 1; r >= 0; r--) {
+				if(isEmptyDisk[r][c]) {
+					copyOfRuinsInfo[r][c] = wall[wallIdx];
+					wallIdx++;
+				}
+			}
+		}
 	}
 
-	private static void exploration() {
-		// TODO Auto-generated method stub
+	private static int[][] copyRuinsInfo() {
 		
+		int[][] copy = new int[RUINSSIZE][RUINSSIZE];
+		
+		for (int r = 0; r < copy.length; r++) {
+			for (int c = 0; c < copy.length; c++) {
+				copy[r][c] = ruinsInfo[r][c];
+			}
+		}
+		
+		return copy;
 	}
-
-
 
 }

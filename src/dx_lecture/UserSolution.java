64e7e6474a9fa688a6a0ae312d987cabaf66d2e6
minju <mj1584.mk@gmail.com>
2025-01-19 15:46:43 +0900

    DX 특강 과제

---
+++ src/dx_lecture/UserSolution.java
@@ -0,0 +1,19 @@
+package dx_lecture;
+
+class UserSolution {
+
+	private final static int MAX_N	= 1000;
+	private final static int MAX_M	= 20;
+
+	public void init(int N, int M, int Map[][]) {
+
+	}
+
+	public Solution.Result findTreasureChest(int Pieces[][]){
+		Solution.Result res = new Solution.Result();
+		res.y = res.x = 0;
+
+		return res;
+	}
+}
+
